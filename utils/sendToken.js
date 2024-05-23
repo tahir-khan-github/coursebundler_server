28fe11838f16dbe56a8ad33e47892019244f14a4
Tahir khan <khan.tahir0032@gmail.com>
2024-05-23 18:49:22 +0530

    correcting cookie option

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -4,8 +4,8 @@ export const sendToken =(res, user, message, statusCode=200)=>{
 
     const options = {
         expires: new Date(Date.now()+ 15 * 24 * 60 * 60 * 1000),
-        // httpOnly: true,
-        // sameSite: false,
+        httpOnly: true,
+        sameSite: true,
         secure: true
     }
 
