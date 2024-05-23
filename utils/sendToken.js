c07ad0efb93c6a4c10f76c2fe0c1ef68ba393598
Tahir khan <khan.tahir0032@gmail.com>
2024-05-23 19:15:48 +0530

    updating cookei option

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,10 +3,10 @@ export const sendToken =(res, user, message, statusCode=200)=>{
     const token = user.getJWTToken();
 
     const options = {
-        expires: new Date(Date.now()+ 15 * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-        sameSite:false,
-        secure: true
+        expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), // 15 days
+        httpOnly: true, // This ensures the cookie is only accessible by the web server
+        sameSite: 'None', // Ensure this matches your front-end and back-end setup, especially if using cross-site requests
+        secure: true // Make sure this is true only in production
     }
 
     res.status(statusCode).cookie("token",token,options).json({
