1eccb061d00338c7115b5d7744921d31545c5c39
Tahir khan <khan.tahir0032@gmail.com>
2024-05-24 12:09:54 +0530

    adjusting old option

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,13 +5,14 @@ import getDataUri from "../utils/dataUri.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { sendEmail } from "../utils/sendEmail.js";
 import { sendToken } from "../utils/sendToken.js";
-import crypto from "crypto";
-import cloudinary from "cloudinary";
+import crypto from "crypto"
+import cloudinary from "cloudinary"
 import { Stats } from "../models/Stats.js";
 
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
   const file = req.file;
+
 
   if (!name || !email || !password || !file) {
     return next(new ErrorHandler("Please add all fields", 400));
@@ -60,15 +61,14 @@ export const login = catchAsyncError(async (req, res, next) => {
 });
 
 export const logout = catchAsyncError(async (req, res, next) => {
-  
-
-  res.status(200).cookie("token", null, {
+  const options = {
     expires: new Date(Date.now()),
-    httpOnly: true, // This ensures the cookie is only accessible by the web server
-    sameSite: "None", // Ensure this matches your front-end and back-end setup, especially if using cross-site requests
-    secure: true, // Make sure this is true only in production
-    path: '/',
-  }).json({
+    httpOnly: true,
+    sameSite: true,
+    secure: true
+  };
+
+  res.status(200).cookie("token", null, options).json({
     success: true,
     message: "logout successfully",
   });
@@ -126,6 +126,7 @@ export const updateProfile = catchAsyncError(async (req, res, next) => {
 });
 
 export const updateProfilePicture = catchAsyncError(async (req, res, next) => {
+
   const user = await User.findById(req.user._id);
 
   const file = req.file;
@@ -138,10 +139,10 @@ export const updateProfilePicture = catchAsyncError(async (req, res, next) => {
 
   user.avatar = {
     public_id: myCloud.public_id,
-    url: myCloud.secure_url,
-  };
+    url: myCloud.secure_url
+  }
 
-  await user.save();
+  await user.save()
 
   res.status(200).json({
     success: true,
@@ -158,7 +159,7 @@ export const forgetPassword = catchAsyncError(async (req, res, next) => {
 
   const resetToken = await user.getResetToken();
 
-  await user.save();
+  await user.save()
 
   // http://localhost:3000/resetpassword/asdasdasdds
 
@@ -189,15 +190,14 @@ export const resetPassword = catchAsyncError(async (req, res, next) => {
     },
   });
 
-  if (!user)
-    return next(new ErrorHandler("token is invalid or has been expired", 401));
+  if (!user) return next(new ErrorHandler("token is invalid or has been expired", 401));
 
   user.password = req.body.password;
 
-  user.resetPassowrdExpire = undefined;
-  user.resetPasswordToken = undefined;
+  user.resetPassowrdExpire = undefined
+  user.resetPasswordToken = undefined
 
-  await user.save();
+  await user.save()
 
   res.status(200).json({
     success: true,
@@ -205,85 +205,87 @@ export const resetPassword = catchAsyncError(async (req, res, next) => {
   });
 });
 
-export const addToPlaylist = catchAsyncError(async (req, res, next) => {
-  const user = await User.findById(req.user._id);
 
-  const course = await Course.findById(req.body.id);
+export const addToPlaylist = catchAsyncError(async (req, res, next) =>{
+    const user = await User.findById(req.user._id);
 
-  if (!course) return next(new ErrorHandler("Invalid course id", 404));
+    const course = await Course.findById(req.body.id);
 
-  const itemExist = user.playlist.find((item) => {
-    if (item.course._id.toString() === course._id.toString()) return true;
-  });
+    if(!course) return next(new ErrorHandler("Invalid course id",404));
 
-  if (itemExist)
-    return next(new ErrorHandler("Course already exist in playlist", 400));
+    const itemExist = user.playlist.find((item)=>{
+        if(item.course._id.toString() === course._id.toString()) return true
+    });
 
-  user.playlist.push({
-    course: course._id,
-    poster: course.poster.url,
-  });
+    if(itemExist) return next(new ErrorHandler("Course already exist in playlist",400))
 
-  await user.save();
+    user.playlist.push({
+        course:course._id,
+        poster:course.poster.url
+    })
 
-  res.status(200).json({
-    success: true,
-    message: "Added to playlist",
-  });
-});
+    await user.save();
 
-export const removeFromPlaylist = catchAsyncError(async (req, res, next) => {
-  const user = await User.findById(req.user._id);
+    res.status(200).json({
+        success: true,
+        message: "Added to playlist",
+      });
+})
 
-  const course = await Course.findById(req.query.id);
+export const removeFromPlaylist = catchAsyncError(async (req, res, next) =>{
+    const user = await User.findById(req.user._id);
 
-  if (!course) return next(new ErrorHandler("Invalid course id", 404));
+    const course = await Course.findById(req.query.id);
 
-  const newPlaylist = user.playlist.filter((item) => {
-    if (item.course._id.toString() !== course._id.toString()) return item;
-  });
+    if(!course) return next(new ErrorHandler("Invalid course id",404));
 
-  user.playlist = newPlaylist;
+   const newPlaylist = user.playlist.filter((item)=>{
+      if(item.course._id.toString() !== course._id.toString()) return item;
+   });
 
-  await user.save();
+   user.playlist = newPlaylist;
 
-  res.status(200).json({
-    success: true,
-    message: "Removed from playlist",
-  });
-});
+    await user.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Removed from playlist",
+      });
+})
 
 //admin controllers
 
-export const getAllUsers = catchAsyncError(async (req, res, next) => {
+export const getAllUsers = catchAsyncError(async (req, res, next) =>{
   const users = await User.find({});
 
   res.status(200).json({
-    success: true,
-    users,
-  });
+      success: true,
+      users
+    });
 });
 
-export const updateUserRole = catchAsyncError(async (req, res, next) => {
+export const updateUserRole = catchAsyncError(async (req, res, next) =>{
   const user = await User.findById(req.params.id);
 
-  if (!user) return next(new ErrorHandler("User not found", 404));
 
-  if (user.role === "user") user.role = "admin";
+  if(!user) return next(new ErrorHandler("User not found",404));
+
+  if(user.role === "user") user.role = "admin";
   else user.role = "user";
 
   await user.save();
 
   res.status(200).json({
-    success: true,
-    message: "role updated succesfully",
-  });
-});
+      success: true,
+      message: "role updated succesfully"
+    });
+})
 
-export const deleteUser = catchAsyncError(async (req, res, next) => {
+export const deleteUser = catchAsyncError(async (req, res, next) =>{
   const user = await User.findById(req.params.id);
 
-  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  if(!user) return next(new ErrorHandler("User not found",404));
 
   await cloudinary.v2.uploader.destroy(user.avatar.public_id);
 
@@ -292,13 +294,14 @@ export const deleteUser = catchAsyncError(async (req, res, next) => {
   await user.deleteOne();
 
   res.status(200).json({
-    success: true,
-    message: "User deleted succesfully",
-  });
-});
+      success: true,
+      message: "User deleted succesfully"
+    });
+})
 
-export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
+export const deleteMyProfile = catchAsyncError(async (req, res, next) =>{
   const user = await User.findById(req.user._id);
+
 
   await cloudinary.v2.uploader.destroy(user.avatar.public_id);
 
@@ -308,9 +311,8 @@ export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
 
   const options = {
     expires: new Date(Date.now()),
-    httpOnly: true, // This ensures the cookie is only accessible by the web server
-    sameSite: 'None', // Ensure this matches your front-end and back-end setup, especially if using cross-site requests
-    secure: true // Make sure this is true only in production
+    httpOnly: true,
+    sameSite: true,
   };
 
   res.status(200).cookie("token", null, options).json({
@@ -319,14 +321,15 @@ export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
   });
 });
 
-User.watch().on("change", async () => {
-  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
 
-  const subscription = await User.find({ "subsription.status": "active" });
+User.watch().on("change",async ()=>{
+  const stats = await Stats.find({}).sort({createdAt:"desc"}).limit(1);
+
+  const subscription = await User.find({"subsription.status":"active"});
 
   stats[0].users = await User.countDocuments();
   stats[0].subscriptions = subscription.length;
   stats[0].createdAt = new Date(Date.now());
 
   await stats[0].save();
-});
+})
